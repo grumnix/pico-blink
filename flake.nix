6c51dba3e265cccd8605cf8c6557e10eb8946633
Ingo Ruhnke <grumbel@gmail.com>
2022-08-24 11:30:08 +0200

    Rename example to blink

--- flake.nix
+++ flake.nix
@@ -4,34 +4,40 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
+
+    pico-sdk_flake.url = "github:grumnix/pico-sdk";
   };
 
-  outputs = { nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, pico-sdk_flake }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        packages = {
-          default = hello;
+        pico-sdk = pico-sdk_flake.packages.${system}.default;
+      in {
+        packages = rec {
+          default = blink;
 
-          hello = pkgs.stdenv.mkDerivation {
-            name = "hello";
+          # blink = pkgs.pkgsCross.arm-embedded.stdenv.mkDerivation {
+          blink = pkgs.stdenv.mkDerivation {
+            name = "blink";
             src = ./.;
 
+            PICO_SDK_PATH="${pico-sdk}/lib/pico-sdk";
+
             cmakeFlags = [
-              "-DPICO_SDK=${pkgs.pico-sdk}/lib/pico-sdk"
+              "-DPICO_SDK_PATH=${pico-sdk}/lib/pico-sdk"
+              "-DCMAKE_C_COMPILER=${pkgs.gcc-arm-embedded}/bin/arm-none-eabi-gcc"
+              "-DCMAKE_CXX_COMPILER=${pkgs.gcc-arm-embedded}/bin/arm-none-eabi-g++"
             ];
 
-            nativeBuildInputs = [
-              pkgs.cmake
-              pkgs.gcc-arm-embedded
-            ];
-
-            buildInputs = [
-              pkgs.pico-sdk
+            nativeBuildInputs = with pkgs; [
+              cmake
+              gcc-arm-embedded
+              python3
             ];
           };
-      };
-    };
+        };
+      }
+    );
 }
 
